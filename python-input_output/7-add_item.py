09b4a89efc6031f12bd94ccad821f5f63d7313bf
MairoPedroIsaac <mairopedroisaac@gmail.com>
2024-03-17 22:48:17 +0100

    debugging

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,31 +1,20 @@
 #!/usr/bin/python3
-"""
-Script that adds all arguments to a Python list,
-and then saves them to a file named add_item.json
-"""
+"""Are you docuemnted ?"""
+
 
 import sys
-import json
-from os import path
 
-# Importing custom functions from previous scripts
-from save_to_json_file import save_to_json_file
-from load_from_json_file import load_from_json_file
+if __name__ == "__main__":
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = __import__('6-load_from_json_file') \
+        .load_from_json_file
 
-# File name for storing the list
-file_name = "add_item.json"
+    try:
+        loadFile = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        loadFile = []
 
-# Check if the file exists
-if not path.exists(file_name):
-    # If the file doesn't exist, create an empty list
-    data = []
-else:
-    # If the file exists, load the data from it
-    data = load_from_json_file(file_name)
-
-# Add command line arguments to the list
-data.extend(sys.argv[1:])
-
-# Save the updated list to the file
-save_to_json_file(data, file_name)
-
+    argc = len(sys.argv)
+    for idx in range(1, argc):
+        loadFile.append(sys.argv[idx])
+    save_to_json_file(loadFile, "add_item.json")
