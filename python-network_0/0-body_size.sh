4f80bb98a834181c6cc63feb4b0f8403c98ba55e
MairoPedroIsaac <mairopedroisaac@gmail.com>
2024-03-24 22:43:07 +0200

    done with debugging

--- python-network_0/0-body_size.sh
+++ python-network_0/0-body_size.sh
@@ -1,21 +1,3 @@
 #!/bin/bash
-
-# Check if URL is provided
-if [ -z "$1" ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
-fi
-
-# Extract the URL from the argument
-URL="$1"
-
-# Send a HEAD request to the URL using curl, get the size of the response body in bytes
-CONTENT_LENGTH=$(curl -sI "$URL" | grep -i "Content-Length:" | awk '{print $2}')
-
-# If Content-Length is empty, handle the case
-if [ -z "$CONTENT_LENGTH" ]; then
-    echo "Content-Length not provided in response headers."
-else
-    echo "$CONTENT_LENGTH"
-fi
-
+# Script that shows the Content-Length from a HTTP request
+curl -sI "$1" | grep "Content-Length:" | cut -d " " -f 2
