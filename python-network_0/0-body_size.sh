9022fe08fc2935564ce962c3f38b2c3e2353ae29
MairoPedroIsaac <mairopedroisaac@gmail.com>
2024-03-24 20:12:19 +0200

    z

--- python-network_0/0-body_size.sh
+++ python-network_0/0-body_size.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 # Script that shows the Content-Length from a HTTP request
-curl -sI "$1" | grep "Content-Length:" | cut -d " " -f 2
+curl -sI "$1" | grep "Content-Length:" | cut -d " " -f #!/bin/bash
+# Get the byte size of the HTTP response header for a given URL.
+curl -s "$1" | wc -c2
+
